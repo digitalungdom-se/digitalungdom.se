b4c6b60a1d853a9fd405c5a562dad3ed5b41f765
ippyson <www.ippyson@gmail.com>
2019-02-16 00:23:00 +0100

    fixed register_check

--- routes/user/register/register_check_email.js
+++ routes/user/register/register_check_email.js
@@ -9,8 +9,8 @@ const checkEmail = require( './../../../models/check' ).checkEmail;
 router.post( '/register_check_email', async function( req, res ) {
   try {
     const email = req.body.email;
-    if ( typeof email != 'string' ) return false;
-    if ( !validator.isEmail( email ) ) return false;
+    if ( typeof email != 'string' ) return { email: false };
+    if ( !validator.isEmail( email ) ) return { email: false };
 
     return res.send( { email: await checkEmail( db, email ) } );
   } catch ( e ) {
--- routes/user/register/register_check_username.js
+++ routes/user/register/register_check_username.js
@@ -8,7 +8,7 @@ const checkUsername = require( './../../../models/check' ).checkUsername;
 router.post( '/register_check_username', async function( req, res ) {
   try {
     const username = req.body.username;
-    if ( typeof username != 'string' ) return false;
+    if ( typeof username != 'string' ) return { username: false };
 
     return res.send( { username: await checkUsername( db, username ) } );
   } catch ( e ) {
