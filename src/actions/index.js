ada8463a7a2b301998688fc703e425560c8a1b6c
ippyson <www.ippyson@gmail.com>
2019-03-02 00:32:55 +0100

    changed state routes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,46 +1,46 @@
 import createAsyncFunction from './createAsyncFunction.js'
 
 export let Auth = {
-	receiveAuth: (response) => ({
-		type: 'RECEIVE_AUTH',
-		response,
-		receivedAt: Date.now()
-	})
+  receiveAuth: ( response ) => ( {
+    type: 'RECEIVE_AUTH',
+    response,
+    receivedAt: Date.now()
+  } )
 }
 
 Auth = {
-	...Auth,
-	...createAsyncFunction('auth', {method: 'POST', route: '/api/auth'}, [Auth.receiveAuth])
+  ...Auth,
+  ...createAsyncFunction( 'auth', { method: 'POST', route: '/api/auth' }, [ Auth.receiveAuth ] )
 }
 
 const fakeResponse = 0 ? {
-	  "type": "success",
-	  "name": "Douglas Bengtsson",
-	  "username": "Nautman"
-		} : null
+  "type": "success",
+  "name": "Douglas Bengtsson",
+  "username": "Nautman"
+} : null
 
 export const Login = {
-	...createAsyncFunction('login', {method: 'POST', route: '/api/login'}, [Auth.receiveAuth], fakeResponse),
+  ...createAsyncFunction( 'login', { method: 'POST', route: '/api/login' }, [ Auth.receiveAuth ], fakeResponse ),
 }
 
 export const Register = {
-	...createAsyncFunction('register', {method: 'POST', route: '/api/register'}, [Auth.receiveAuth], fakeResponse),
-	...createAsyncFunction('check_email', {method: 'GET', route: '/api/register_check_email'}, []),
-	...createAsyncFunction('check_username', {method: 'GET', route: '/api/register_check_username'}, []),
+  ...createAsyncFunction( 'register', { method: 'POST', route: '/api/register' }, [ Auth.receiveAuth ], fakeResponse ),
+  ...createAsyncFunction( 'check_email', { method: 'GET', route: '/api/register_check_email' }, [] ),
+  ...createAsyncFunction( 'check_username', { method: 'GET', route: '/api/register_check_username' }, [] ),
 }
 
 export const Users = {
-	...createAsyncFunction('get_user', {method: 'POST', route: '/api/get_user'}, []),
+  ...createAsyncFunction( 'get_user', { method: 'POST', route: '/api/get_user' }, [] ),
 }
 
 export const Agora = {
-	...createAsyncFunction('agora_publish_post', {method: 'POST', route: '/api/agorize'}, []),
-	...createAsyncFunction('agora_publish_link', {method: 'POST', route: '/api/agorize'}, []),
-	...createAsyncFunction('agora_publish_question', {method: 'POST', route: '/api/agorize'}, []),
-	...createAsyncFunction('agora_publish_comment', {method: 'POST', route: '/api/agorize'}, []),
+  ...createAsyncFunction( 'agora_publish_post', { method: 'POST', route: '/api/agorize' }, [] ),
+  ...createAsyncFunction( 'agora_publish_link', { method: 'POST', route: '/api/agorize' }, [] ),
+  ...createAsyncFunction( 'agora_publish_question', { method: 'POST', route: '/api/agorize' }, [] ),
+  ...createAsyncFunction( 'agora_publish_comment', { method: 'POST', route: '/api/agorize' }, [] ),
 
-	...createAsyncFunction('anti_agorize', {method: 'POST', route: '/api/antiagorize'}, []),
-	...createAsyncFunction('meta_agorize', {method: 'POST', route: '/api/metaagorize'}, []),
+  ...createAsyncFunction( 'anti_agorize', { method: 'POST', route: '/api/anti_agorize' }, [] ),
+  ...createAsyncFunction( 'meta_agorize', { method: 'POST', route: '/api/meta_agorize' }, [] ),
 
-	...createAsyncFunction('get_agoragrams', {method: 'GET', route: '/api/getagoragrams'}, []),
+  ...createAsyncFunction( 'get_agoragrams', { method: 'GET', route: '/api/get_agoragrams' }, [] ),
 }
