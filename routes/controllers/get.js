c56ddf35ff1bed8a42fa7ee4cd9c22b9e349c5cc
ippyson <www.ippyson@gmail.com>
2019-04-19 18:44:55 +0200

    added get publicuser by username

--- routes/controllers/get.js
+++ routes/controllers/get.js
@@ -1,16 +1,30 @@
 /* global include */
 
 const validateObjectID = require( 'mongodb' ).ObjectID.isValid;
+const validator = require( 'validator' );
 
 const getPublicUserById = include( 'models/get' ).getPublicUserById;
+const getPublicUserByUsername = include( 'models/get' ).getPublicUserByUsername;
 const getMemberAmount = include( 'models/get' ).getMemberAmount;
 
-module.exports.getPublicUserById = async function ( req, res ) {
+module.exports.getPublicUser = async function ( req, res ) {
   let userId = req.query.userId;
+  let username = req.query.username;
 
-  if ( userId && !validateObjectID( userId ) ) return res.status( 400 ).send( { 'type': 'fail', 'reason': 'invalid user id', userId } );
+  let user;
+  if ( userId ) {
+    // Validates userId to ensure that it is an objectId
+    if ( !validateObjectID( userId ) ) return res.status( 400 ).send( { 'type': 'fail', 'reason': 'invalid user id', userId } );
 
-  const user = await getPublicUserById( userId );
+    user = await getPublicUserById( userId );
+  } else if ( username ) {
+    // Validates username according to following rules: min 3 max 24 characters and only includes valid characters (A-Z, a-z, 0-9, and _)
+    if ( !validator.isLength( username, { min: 3, max: 24 } ) ) return res.status( 400 ).send( { 'type': 'fail', 'reason': 'Username length is either too long or too short', 'username': username } );
+    if ( !/^(\w+)$/.test( username ) ) return res.status( 400 ).send( { 'type': 'fail', 'reason': 'Invalid characters in username', 'username': username } );
+
+    user = await getPublicUserByUsername( username );
+  }
+
   if ( user ) {
     return res.send( { 'type': 'success', user } );
   } else {
