928ffd65d4b56f758a4e1ebeb079e4be49b84f60
Nautman <doug.beng-2019@vrg.se>
2019-04-19 13:01:02 +0200

    Deleted switch.js system

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ const pack = require('./package.json')
 /* config-overrides.js */
 module.exports = function override(config, env) {
     config = rewireAliases.aliasesOptions({
-        '@components': path.resolve(__dirname, `${paths.appSrc}/${pack.type}_components/`)
+        '@components': path.resolve(__dirname, `${paths.appSrc}/dev_components/`)
     })(config, env);
   return config;
 }
