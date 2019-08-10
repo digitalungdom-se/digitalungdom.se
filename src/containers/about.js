6cf3decea0cd4d514c2f70e13d0b20819b95874d
Charles <charles.maddock@outlook.com>
2019-08-10 19:59:01 +0200

    Småändringar till aboutsidan

--- src/containers/about.js
+++ src/containers/about.js
@@ -2,6 +2,38 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Col, Row, Button, Icon } from 'antd'
 
+/*
+<Row type="flex" justify="center" style={{paddingTop: 80}}>
+
+  <Col span={11}>
+    <div style={{paddingRight: 50}}>
+
+      <h3 style={{fontSize: 24, color: "#434343", fontWeight: 'bold'}}>Vår story</h3>
+
+      <p style={{fontSize: 15, marginBottom: 40, color: "#434343", textAlign:"justify"}}>
+      Digital Ungdom har startat på initiativ från Douglas Bengtsson som är förbundsstyrelsens ordförande.
+      Douglas har länge inspirerats av det nationella förbundet Astronomisk Ungdom, och har saknat en
+      motsvarighet för teknik- och programmeringsintresserade ungdomar. Sommaren 2018 deltog Douglas
+      i den svenska sommarforskarskolan Rays, där han fick träffa andra likasinnade och engagerade ungdomar.
+      Under Rays träffade han bland andra de två engagerade eleverna Simon Sondén och Kelvin Szolnoky.
+      Simon Sondén som är vice ordförande i styrelsen bor i Umeå och har bland annat deltagit i Robocup
+      Junior VM. Kelvin Szolnoky som är vice ordförande i styrelsen bor i Göteborg och har bland annat
+      varit mycket aktiv inom Astronomisk Ungdoms verksamhet.
+      </p>
+
+    </div>
+  </Col>
+
+  <Col span={13}>
+    <div style={{ backgroundColor: 'pink', position: 'relative', height: 400, textAlign: 'center', paddingTop: 160}}>
+      <h1>Mer bilder här på teamet kanske</h1>
+    </div>
+  </Col>
+
+</Row>
+*/
+
+
 export default () => (
 
   <Row type="flex" justify="center"  style={{flex: 1}}>
@@ -38,35 +70,6 @@ export default () => (
   			</Col>
 
   		</Row>
-
-      <Row type="flex" justify="center" style={{paddingTop: 80}}>
-
-        <Col span={11}>
-          <div style={{paddingRight: 50}}>
-
-            <h3 style={{fontSize: 24, color: "#434343", fontWeight: 'bold'}}>Vår story</h3>
-
-            <p style={{fontSize: 15, marginBottom: 40, color: "#434343", textAlign:"justify"}}>
-            Digital Ungdom har startat på initiativ från Douglas Bengtsson som är förbundsstyrelsens ordförande.
-            Douglas har länge inspirerats av det nationella förbundet Astronomisk Ungdom, och har saknat en
-            motsvarighet för teknik- och programmeringsintresserade ungdomar. Sommaren 2018 deltog Douglas
-            i den svenska sommarforskarskolan Rays, där han fick träffa andra likasinnade och engagerade ungdomar.
-            Under Rays träffade han bland andra de två engagerade eleverna Simon Sondén och Kelvin Szolnoky.
-            Simon Sondén som är vice ordförande i styrelsen bor i Umeå och har bland annat deltagit i Robocup
-            Junior VM. Kelvin Szolnoky som är vice ordförande i styrelsen bor i Göteborg och har bland annat
-            varit mycket aktiv inom Astronomisk Ungdoms verksamhet.
-            </p>
-
-          </div>
-        </Col>
-
-        <Col span={13}>
-          <div style={{ backgroundColor: 'pink', position: 'relative', height: 400, textAlign: 'center', paddingTop: 160}}>
-            <h1>Mer bilder här på teamet kanske</h1>
-          </div>
-        </Col>
-
-      </Row>
 
       <Row type="flex" justify="center" style={{paddingTop: 80, width: '100%'}}>
         <h3 style={{fontSize: 24, color: "#434343", fontWeight: 'bold'}}>Teamet 😍</h3>
