e386575e8dd211f6b689a90595d3cfe7d9a8da8d
Charles <charles.maddock@outlook.com>
2019-08-15 10:00:44 +0200

    Försökt färgskilja segment på hemsidan, la även till en arbete-pågår-sektion.

--- src/prod_components/home.js
+++ src/prod_components/home.js
@@ -9,65 +9,114 @@ const fontSize = {
 	md: 40,
 }
 
+const CenterWrapper = ({ children }) => (
+	<Row type = "flex" justify="center">
+		<Col type = "flex" justify="center" span={20}>
+			{children}
+		</Col>
+	</Row>
+)
+
 export default () => (
 
-	<Col type="flex" justify="space-between" style={{paddingTop: 60}}>
-		<Row gutter = {30} type="flex" justify="space-between" align="middle">
-			<Col
-				sm={{span: 24}}
-				md={{span: 14}}
-			>
-				<div>
-					<img src={require("resources/images/FrontPage.png")}  style={{width: "100%"}}/>
-				</div>
-			</Col>
+	<Col type="flex" justify="space-between">
+		<Row style={{background: "#05379c", paddingTop: 60, paddingBottom: 50, width: "100%"}}>
+			<CenterWrapper>
+				<Row type="flex" justify="space-between" align="middle">
+					<Col
+						sm={{span: 24}}
+						md={{span: 14}}
+					>
+						<div>
+							<img src={require("resources/images/FrontPage.png")}  style={{width: "100%"}}/>
+						</div>
+					</Col>
 
-			<Col
-				xs={{span: 20}}
-				sm={{span: 15}}
-				md={{span: 10}}
-			>
-				<div>
+					<Col
+						xs={{span: 20}}
+						sm={{span: 15}}
+						md={{span: 10}}
+					>
+						<div>
 
-					<h1 style={{fontSize: 40, marginBottom: 10, color: 'black', fontWeight: 'bold'}}>Programmering är roligt!</h1>
+							<h1 style={{fontSize: 40, marginBottom: 10, color: 'white', fontWeight: 'bold'}}>Programmering är roligt!</h1>
 
-					<h3 style={{fontSize: 30, color: "#434343", fontWeight: 'bold'}}>Här kan du lära dig, <br/> dela med dig av och hjälpa<br/> andra med programmering. </h3>
+							<h3 style={{fontSize: 30, color: "rgba(255,255,255,0.9)", fontWeight: 'bold'}}>Här kan du lära dig, <br/> dela med dig av och hjälpa<br/> andra med programmering. </h3>
 
-					<p style={{fontSize: 18, marginBottom: 40, color: "#434343"}}>
-						Vi är ett svenskt ideellt ungdomsförbund för programmerings- och teknikintresserade ungdomar. Bli medlem och ta del av vår community! 🌟
-					</p>
+							<p style={{fontSize: 18, marginBottom: 40, color: "rgba(255,255,255,0.9)"}}>
+								Vi är ett svenskt ideellt ungdomsförbund för programmerings- och teknikintresserade ungdomar. Bli medlem och ta del av vår community! 🌟
+							</p>
 
-					<Row>
-						<Col span={8}>
-							<Link to="register">
-								<Button
-								style={{ width: '90%'}}
-								size="large"
-								type="primary">
-									Bli medlem!
-								</Button>
-							</Link>
-						</Col>
+							<Row>
+								<Col span={8}>
+									<Link to="register">
+										<Button
+										style={{ width: '90%'}}
+										size="large"
+										type="primary">
+											Bli medlem!
+										</Button>
+									</Link>
+								</Col>
 
-						<Col span={8}>
-							<Link to="login">
-								<Button ghost size="large" style={{color: "#434343", width: '90%', border: "2px solid #cccccc"}}>
-									Logga in
-								</Button>
-							</Link>
-						</Col>
+								<Col span={8}>
+									<Link to="login">
+										<Button ghost size="large" style={{color: "rgba(255,255,255,0.9)", width: '90%', border: "2px solid #cccccc"}}>
+											Logga in
+										</Button>
+									</Link>
+								</Col>
 
-						<Col span={8}>
-							<Link to="about">
-								<Button ghost size="large" style={{color: "#434343",width: '90%', border: "2px solid #cccccc"}}>
-									Om oss
-								</Button>
-							</Link>
-						</Col>
-					</Row>
+								<Col span={8}>
+									<Link to="about">
+										<Button ghost size="large" style={{color: "rgba(255,255,255,0.9)",width: '90%', border: "2px solid #cccccc"}}>
+											Om oss
+										</Button>
+									</Link>
+								</Col>
+							</Row>
 
-				</div>
-			</Col>
+						</div>
+					</Col>
+				</Row>
+			</CenterWrapper>
+		</Row>
+		<div style={{background: "#05379c", borderBottomLeftRadius: "50%", borderBottomRightRadius: "50%", height: 20, width: "100%"}}/>
+
+		<Row type="flex" justify="center" style={{flex: 1}}>
+			<Row type="flex" justify="center" style={{paddingTop: 140, paddingBottom: 80}}>
+				<Col span={8}>
+					<div>
+						<img src={require("resources/images/underConstruction.png")} style={{height: 330, marginTop: -50}}/>
+					</div>
+				</Col>
+
+				<Col span={8}>
+					<div>
+
+						<h3 style={{fontSize: 28, color: "#434343", fontWeight: 'bold'}}>Arbete pågår!</h3>
+
+						<p style={{fontSize: 16, marginBottom: 10, color: "#434343"}}>
+							Detta är den första versionen av digitalungdom.se så det saknas forfarande många planerade funktioner.
+							Vi arbetar dock hårt med hemsidan och fler funktioner kommer snart!
+						</p>
+
+						<p style={{fontSize: 16, marginBottom: 10, color: "#434343"}}>
+							För tillfället kan ni njuta av Agora (vårt forum) och
+							faktumet att ni kan skapa i princip vilket användarnamn som helst! 😎
+						</p>
+
+						<Row style={{marginTop: 30}}>
+								<Link to="register">
+									<Button type="primary" size="large" style={{ borderRadius: 10 }}>
+										Skapa ett konto
+									</Button>
+								</Link>
+						</Row>
+
+					</div>
+				</Col>
+			</Row>
 		</Row>
 	</Col>
 )
