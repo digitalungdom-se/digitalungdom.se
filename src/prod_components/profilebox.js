b300ea29892b904bf4d59fa58f2fbc44f06e8fae
kelszo <www.ippyson@gmail.com>
2019-08-16 19:39:29 +0200

    moving cards

--- src/prod_components/profilebox.js
+++ src/prod_components/profilebox.js
@@ -3,6 +3,8 @@ import React from 'react'
 // import Button from '@components/button'
 import { Menu, Dropdown, Button, Row } from 'antd'
 import Link from '@components/link'
+import ProfilePicture from '@components/ProfilePicture'
+
 
 const menu = () => (
   <menu>
@@ -18,11 +20,7 @@ export default ( { profile, logOut, translations } ) => (
   <Dropdown overlay = { menu } trigger = {[ 'click' ] }>
     <Row >
       <Link loading = { profile.authing } style = { { width: "100%" } } to = { "/@" + profile.details.username } type = "button" >
-        {
-          profile.details.username ?
-          profile.details.username :
-            'Loading...'
-        }
+        <ProfilePicture style={{position: 'absolute'}} username={profile.details.username} size={36}/>
       </Link>
     </Row>
   </Dropdown>
