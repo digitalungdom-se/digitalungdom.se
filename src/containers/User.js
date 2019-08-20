f4175f400ae841a0df97db2e699f984d2bb673fe
kelszo <www.ippyson@gmail.com>
2019-08-20 17:29:07 +0200

    styling

--- src/containers/User.js
+++ src/containers/User.js
@@ -4,17 +4,22 @@ import { getUser } from 'actions/users'
 import UserPage from '@components/UserPage'
 import { Row, Col } from '@components/grid'
 
-function UserContainer({ username }) {
+function UserContainer( { username } ) {
 
-	const user = useSelector(state => state.Users.usernames[username])
-	if(user === undefined) {
-		const dispatch = useDispatch()
-		dispatch(getUser({username}))
-	}
+  const user = useSelector( state => state.Users.usernames[ username ] );
+  if ( !user ) {
+    const dispatch = useDispatch()
+    dispatch( getUser( { username } ) )
+  }
 
-	return (
-		<UserPage loading={!user} user={user} />
-	)
+  const authenticatedUserID = useSelector( state => state.Auth.profile.details._id );
+
+  let canEdit = false;
+  if ( user && authenticatedUserID && user._id === authenticatedUserID ) canEdit = true;
+
+  return (
+    <UserPage loading={!user} user={user} canEdit={canEdit} />
+  )
 }
 
 export default UserContainer
