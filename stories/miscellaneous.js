caf7bd839762d7c1c9d0618310d8f33b729e75dc
Nautman <doug.beng-2019@vrg.se>
2019-08-25 10:39:21 +0200

    storybook addons

--- stories/miscellaneous.js
+++ stories/miscellaneous.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react';
 import Empty from '@components/Empty'
+import ProfileDropdown from '@components/ProfileDropdown'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 storiesOf('Miscellaneous', module)
+	.addDecorator(withKnobs)
 	.add("Empty", () => (
 		<Empty
 			description="Inga inlägg hittades med detta filter!"
 		/> 
 	))
+	.add("Profile dropdown", () => {
+		const name = text('Name', 'Arunoda Susiripala');
+		const age = number('Age', 89);
+
+		return (
+			<ProfileDropdown
+				name={name}
+				age={age}
+			/> 
+		)}
+	)
