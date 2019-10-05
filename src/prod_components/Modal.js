734630b8afb0ca211d240bdfb9d7540ef668bf48
Nautman <doug.beng-2019@vrg.se>
2019-10-05 17:01:19 +0200

    Modal

---
+++ src/prod_components/Modal.js
@@ -0,0 +1,66 @@
+import React, { createRef } from 'react'
+import { Modal, Col, Row, Button, Input } from 'antd'
+
+function DU_Modal({
+  visible = false,
+  handleCancel = () => {},
+  handleConfirm = () => {},
+  title = "",
+  description = "",
+  cancelText = "Cancel",
+  confirmText = "Confirm",
+  showInput = false,
+  handleInput = () => {},
+  onCancel = () => {},
+  onConfirm = () => {}
+}) {
+
+  const inputRef = createRef()
+
+  return (
+     <Modal
+       visible={visible}
+       onCancel={onCancel}
+       footer={null}
+       style={{textAlign: "center", maxWidth: 300}}
+     >
+       <h1 style={{fontSize: 18, fontWeight: 'bold'}}>{title}</h1>
+       <p>{description}</p>
+       {
+        showInput &&
+        <Input
+          ref={inputRef}
+        />
+       }
+       <Row type="flex" justify="center" gutter={16}>
+         <Col span={12}>
+           <Button
+             style={{width: "100%"}}
+             onClick={() => {
+              onCancel()
+              handleCancel()
+            }}
+           >
+             {cancelText}
+           </Button>
+         </Col>
+         <Col span={12}>
+           <Button
+             style={{width: "100%"}}
+             type="danger"
+             onClick={() =>
+               {
+                onConfirm()
+                if(showInput) handleConfirm(inputRef.current.input.value);
+                else handleConfirm();
+               }
+             }>
+             {confirmText}
+           </Button>
+         </Col>
+       </Row>
+     </Modal>
+  )
+}
+
+export default DU_Modal
