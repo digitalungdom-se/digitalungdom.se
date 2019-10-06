4d13b1badf85fd0e58c32dcbf6dbff6ba89cea49
Nautman <doug.beng-2019@vrg.se>
2019-10-06 19:10:34 +0200

    Fixes to modal, post, and user

--- src/containers/User.js
+++ src/containers/User.js
@@ -20,66 +20,71 @@ function UserContainer( { username } ) {
 
   // Find the userId of the current userpage, given the username
   const userId = useSelector( state => state.Users.usernames[ username ]);
-  // if the userId is "LOADING" or undefined, then there is no available information on that user!
-  // hence, return a loading page for the user
-  if(userId === "LOADING" || userId === undefined) {
-    return <Loading />
-  }
-  // If the userId IS defined, then there is information on that user!
-  else {
-    // Find the user from the "users" object.
-    // It looks something like this:
-    /*
-    users = {
-      "507f191e810c19729de860ea": {
-        _id: "507f191e810c19729de860ea",
-        profile: {
-          username: "Nautman",
-          etc...
-        }
-      },
-      "123d191e810c19729de860ea": {
-        _id: "123d191e810c19729de860ea",
-        profile: {
-          username: "Charles",
-          etc...
-        }
+  // Find the user from the "users" object.
+  // It looks something like this:
+  /*
+  users = {
+    "507f191e810c19729de860ea": {
+      _id: "507f191e810c19729de860ea",
+      profile: {
+        username: "Nautman",
+        etc...
+      }
+    },
+    "123d191e810c19729de860ea": {
+      _id: "123d191e810c19729de860ea",
+      profile: {
+        username: "Charles",
+        etc...
       }
     }
-    */
-    const user = useSelector( state => state.Users.users[ userId ]);
-
-    const deleteUserFromModal = userID => dispatch(deleteUser({userID}))
-
-    const authenticatedUserID = useSelector( state => state.Auth.profile.details._id)
-
-    let canEdit = false;
-    let edit = function () {};
-
-    if ( user && authenticatedUserID && user._id === authenticatedUserID ) {
-      canEdit = true;
-      const userID = user._id;
-
-      edit = ( info, userID ) => dispatch( set( info, userID ) )
-    }
-
-    //let report = report(id) {dispatch(reportUser(user._id))}
-
-    let agoragrams = useSelector(state => state.Agora.lists[username])
-
-    /*
-      TO-DO: Make a special loading page for the user by using a <Skeleton> from antd.
-    */
-    return (
-      <UserPage
-        user={user}
-        canEdit={canEdit}
-        edit={edit}
-        posts={<Posts list={agoragrams} />}
-        deleteUserFromModal={deleteUserFromModal}
-      />
-    )
   }
+  */
+  const user = useSelector( state => state.Users.users[ userId ]);
+
+  // if the userId is "LOADING" or undefined, then there is no available information on that user!
+  const loading = (userId === undefined || userId == "LOADING")
+
+  // If the userId IS defined, then there is information on that user!
+
+  const deleteUserFromModal = userID => dispatch(deleteUser({userID}))
+
+  const authenticatedUserID = useSelector( state => state.Auth.profile.details._id)
+
+  let canEdit = false;
+  let edit = function () {};
+
+  if ( user && authenticatedUserID && user._id === authenticatedUserID ) {
+    canEdit = true;
+    const userID = user._id;
+
+    edit = ( info, userID ) => dispatch( set( info, userID ) )
+  }
+
+  //let report = report(id) {dispatch(reportUser(user._id))}
+
+  let agoragrams = useSelector(state => state.Agora.lists[username])
+
+  if(loading) {
+    return <Loading spin card />
+  }
+
+  if(userId === false) {
+    return <div>No such user</div>
+  }
+
+  /*
+    TO-DO: Make a special loading page for the user by using a <Skeleton> from antd.
+  */
+  return (
+    <UserPage
+      user={user}
+      canEdit={canEdit}
+      edit={edit}
+      posts={<Posts list={agoragrams} />}
+      deleteUserFromModal={deleteUserFromModal}
+    />
+  )
 }
 
 export default UserContainer
