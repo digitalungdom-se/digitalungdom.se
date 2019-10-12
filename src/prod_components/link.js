ea0ca5bae91f8964f38f46587eb5ca72f547e80f
kelszo <www.ippyson@gmail.com>
2019-10-12 13:49:04 +0200

    username is now displayed in the users colour

--- src/prod_components/link.js
+++ src/prod_components/link.js
@@ -3,29 +3,28 @@ import { Link, NavLink } from 'react-router-dom'
 // import { Button } from '@components'
 import { Button } from 'antd'
 
-export default ({ children, to = "", linkType, onClick, id, user, ...props}) => {
-	switch(linkType) {
-		case 'button':
-			return (
-				<Link onClick={onClick} to={to} >
+export default ( { children, to = "", linkType, onClick, id, user, ...props } ) => {
+  switch ( linkType ) {
+  case 'button':
+    return (
+      <Link onClick={onClick} to={to} >
 					<Button {...props} >
 						{children}
 					</Button>
 				</Link>
-			)
-		case 'user':
-			if(user) return (
-				!user.details ?
-					<code>[deleted]</code>
-				:
-					<Link to={"/@" + user.details.username} {...props}>
+    )
+  case 'user':
+    if ( user ) return (
+      !user.details ?
+      <code>[deleted]</code> :
+      <Link to={"/@" + user.details.username} {...props}>
 						{user.details.name}
 					</Link>
-			);
-			else return <code>[deleted]</code>
-		case 'navigation':
-			return(
-				<NavLink
+    );
+    else return <code>[deleted]</code>
+  case 'navigation':
+    return (
+      <NavLink
 				onClick={onClick}
 				to={to}
 				style={{color: "grey"}}
@@ -35,12 +34,12 @@ export default ({ children, to = "", linkType, onClick, id, user, ...props}) => 
 				{...props}>
 					{children}
 				</NavLink>
-			)
-		default:
-			return (
-				<Link onClick={onClick} to={to} {...props}>
+    )
+  default:
+    return (
+      <Link onClick={onClick} to={to} {...props}>
 					{children}
 				</Link>
-			)
-	}
+    )
+  }
 }
