13cabad2b9604ce69d75f6dd11dc06514d55759e
kelszo <www.ippyson@gmail.com>
2019-10-12 19:51:51 +0200

    fix colour

--- src/containers/Link.js
+++ src/containers/Link.js
@@ -3,22 +3,27 @@ import { useSelector } from 'react-redux'
 import Link from '@components/link'
 
 export default ( { linkType, id, user, ...props } ) => {
+
   switch ( linkType ) {
   case 'user':
     const user = useSelector( state => state.Users.users[ id ] )
-    if ( user ) return (
-      !user.details ?
-      <Link
+    if ( user ) {
+      var colour;
+      if ( user && user.profile && user.profile.colour ) colour = user.profile.colour
+      return (
+        !user.details ?
+        <Link
 						linkType={linkType}
 						id={id}
 						{...props}
 					>
 						[deleted]
 					</Link> :
-      <Link to={"/@" + user.details.username} {...props} style={{color:user.profile.colour ? user.profile.colour : '#83aef2'}}>
+        <Link to={"/@" + user.details.username} {...props} style={{color: colour ? colour : '#83aef2'}}>
 					{user.details.name}
 				</Link>
-    );
+      );
+    }
     // TO-DO: Add "Loading..." if the id is defined but not loading. Remember to check the case where the account has been deleted.
     else return <code>[raderad]</code>
   default:
