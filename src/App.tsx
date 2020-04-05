24487028be09efcd938a45124abc5da3e7e9533a
Nautman <douglas@digitalungdom.se>
2020-04-05 22:33:20 +0200

    Added initial eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
