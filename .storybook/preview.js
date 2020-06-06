72b0d1b42c6317ca89b99e5eb09ffc3433cd53a8
Nautman <douglas@digitalungdom.se>
2020-06-06 16:35:05 +0200

    Setup storybook-react-router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import StoryRouter from 'storybook-react-router';
 import ThemeLayer from './../src/components/ThemeLayer';
 import { addDecorator } from '@storybook/react';
 
+addDecorator(StoryRouter());
 addDecorator(storyFn => <ThemeLayer>{storyFn()}</ThemeLayer>);
