30505675e7df3306f91609aff41b76b72069491d
Nautman <douglas@digitalungdom.se>
2020-06-08 00:57:59 +0200

    Added and implemented storybook-dark-mode
    It is now possible to view the Storybook in dark mode, and see how components look in dark mode. Neat!

---
+++ .storybook/addons.js
@@ -0,0 +1,1 @@
+import 'storybook-dark-mode/register';
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,18 @@ import React from 'react'
 import StoryRouter from 'storybook-react-router';
 import ThemeLayer from './../src/components/ThemeLayer';
 import { addDecorator } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+import { useDarkMode } from 'storybook-dark-mode';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+addParameters({
+  darkMode: {
+    dark: themes.dark,
+    light: themes.normal,
+    current: 'light'
+  }
+});
 
 addDecorator(StoryRouter());
-addDecorator(storyFn => <ThemeLayer>{storyFn()}</ThemeLayer>);
+addDecorator(storyFn => <ThemeLayer forceDarkMode={useDarkMode() ? 'dark' : 'light'}>{storyFn()}</ThemeLayer>);
