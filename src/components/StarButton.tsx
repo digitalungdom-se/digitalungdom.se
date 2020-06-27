dfb6f10b4fd3f6f25ea334d71dccac14a1fef712
Nautman <douglas@digitalungdom.se>
2020-06-27 12:59:51 +0200

    New export on StarButton + className prop
    * StarButton now has a second export (StarButton), and it's primary export is StarIconButton.
    
    * StarButton has the className prop (applied to the Button / IconButton component).

--- src/components/StarButton.tsx
+++ src/components/StarButton.tsx
@@ -1,3 +1,4 @@
+import Button, { ButtonProps } from '@material-ui/core/Button';
 import Confetti, { ConfettiConfig } from 'react-dom-confetti';
 
 import IconButton from '@material-ui/core/IconButton';
@@ -12,22 +13,45 @@ const useStyles = makeStyles({
   },
 });
 
-export interface StarButtonProps {
+export interface StarButtonProps extends ButtonProps {
+  className?: string;
+  confettiConfig?: ConfettiConfig;
   /**
    * The fontSize applied to the icon. Defaults to 24px, but can be configure to inherit font size.
    */
   fontSize?: 'inherit' | 'default' | 'small' | 'large';
-  confettiConfig?: ConfettiConfig;
 }
 
-export default function StarButton({ fontSize = 'large', confettiConfig }: StarButtonProps): React.ReactElement {
+export default function StarIconButton({
+  className,
+  confettiConfig,
+  fontSize = 'large',
+}: StarButtonProps): React.ReactElement {
   const [clicked, setClick] = React.useState<boolean>(false);
   const handleClick = (): void => setClick(!clicked);
   const classes = useStyles({ clicked });
   return (
-    <IconButton onClick={handleClick}>
+    <IconButton className={className} onClick={handleClick}>
       <Confetti active={clicked} config={confettiConfig} />
       <SvgIcon className={classes.star} component={StarIcon} fontSize={fontSize} />
     </IconButton>
+  );
+}
+
+export function StarButton({
+  className,
+  children,
+  confettiConfig,
+  fontSize = 'large',
+}: StarButtonProps): React.ReactElement {
+  const [clicked, setClick] = React.useState<boolean>(false);
+  const handleClick = (): void => setClick(!clicked);
+  const classes = useStyles({ clicked });
+  return (
+    <Button className={className} onClick={handleClick}>
+      <Confetti active={clicked} config={confettiConfig} />
+      <SvgIcon className={classes.star} component={StarIcon} fontSize={fontSize} />
+      {children}
+    </Button>
   );
 }
