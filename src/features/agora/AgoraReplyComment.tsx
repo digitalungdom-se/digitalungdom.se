40f3cb6c96c6c4fadcdc135e11bd63a3557f8522
Nautman <28629647+Nautman@users.noreply.github.com>
2020-07-25 10:12:20 +0200

    Added alert to ask user to log in when commenting

--- src/features/agora/AgoraReplyComment.tsx
+++ src/features/agora/AgoraReplyComment.tsx
@@ -1,9 +1,13 @@
 import { Field, Form, Formik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
 import Axios from 'axios';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { TextField } from 'formik-material-ui';
 import { newCommentSuccess } from './agoraSlice';
 import { selectMyProfile } from 'features/auth/authSlice';
@@ -11,6 +15,21 @@ import { selectMyProfile } from 'features/auth/authSlice';
 export default function AgoraReplyComment({ replyTo }: { replyTo: string }) {
   const dispatch = useDispatch();
   const myProfile = useSelector(selectMyProfile);
+  if (myProfile === null)
+    return (
+      <Alert severity="info" variant="outlined">
+        <AlertTitle>Express yourself!</AlertTitle>
+        <Box flexGrow={1}>You need to be logged in to make a comment.</Box>
+        <Box>
+          <Button color="primary" component={RouterLink} disableElevation to="/bli-medlem" variant="contained">
+            Sign up
+          </Button>
+          <Button component={RouterLink} disableElevation to="/logga-in" variant="contained">
+            Log in
+          </Button>
+        </Box>
+      </Alert>
+    );
   return (
     <Formik
       initialValues={{ body: '' }}
