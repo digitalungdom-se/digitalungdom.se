6f4ec56ab7829facb5f9df02c89e2aafe2812a18
Nautman <28629647+Nautman@users.noreply.github.com>
2020-11-08 18:54:29 +0100

    Added agoragrams to user page

--- src/features/users/index.tsx
+++ src/features/users/index.tsx
@@ -1,10 +1,14 @@
 import { ConnectedProps, connect } from 'react-redux';
 
+import { Agoragram } from 'features/agora/agoraTypes';
 import Axios from 'axios';
+import { Grid } from '@material-ui/core';
 import { ProfileCard } from './ProfileCard';
 import React from 'react';
+import { ReduxConnectedPost } from 'features/agora/AgoraPost';
 import { RootState } from 'app/store';
 import { User } from './userTypes';
+import { getAgoragramsSuccess } from 'features/agora/agoraSlice';
 import { getUsersSuccess } from './usersSlice';
 import { selectMyId } from 'features/auth/authSlice';
 
@@ -12,11 +16,13 @@ interface ProfilePageState {
   loading: boolean;
   error: boolean;
   user: User | null;
+  agoragrams: Agoragram[];
 }
 
 interface ServerResponse {
   user: User;
   users: User[];
+  agoragrams: Agoragram[];
 }
 
 const mapState = (state: RootState) => ({
@@ -25,6 +31,7 @@ const mapState = (state: RootState) => ({
 
 const mapDispatch = {
   getUsersSuccess,
+  getAgoragramsSuccess,
 };
 
 const connector = connect(mapState, mapDispatch);
@@ -39,6 +46,7 @@ class ProfilePage extends React.Component<ProfilePageProps, ProfilePageState> {
   constructor(props: ProfilePageProps) {
     super(props);
     this.state = {
+      agoragrams: [],
       error: false,
       loading: false,
       user: null,
@@ -57,45 +65,58 @@ class ProfilePage extends React.Component<ProfilePageProps, ProfilePageState> {
     })
       .then((res) => {
         this.props.getUsersSuccess(res.data.users);
-        this.setState({ user: res.data.user });
+        this.props.getAgoragramsSuccess(res.data);
+        this.setState({
+          agoragrams: res.data.agoragrams,
+          user: res.data.user,
+        });
       })
       .catch((err) => this.setState({ error: true }));
   }
 
   render() {
-    const { user } = this.state;
+    const { user, agoragrams } = this.state;
     if (user == null) return <div>Loading</div>;
     return (
-      <ProfileCard
-        bio={user.profile.bio}
-        firstName={user.details.name.split(' ')[0]}
-        isOwner={this.props.myId === user._id}
-        joinDate={new Date(parseInt(user._id.substring(0, 8), 16) * 1000)}
-        lastName={user.details.name.split(' ')[1]}
-        onSubmit={(values, { setSubmitting }): void => {
-          const array: Array<[string, Record<string, unknown>]> = [];
-          for (const [key, value] of Object.entries(values)) {
-            array.push(['profile.' + key, { [key]: value }]);
-          }
-          Axios.put('/api/user/set', {
-            updates: array,
-          }).then((res) => {
-            setSubmitting(false);
-            this.setState({
-              user: {
-                ...user,
-                profile: {
-                  ...user.profile,
-                  ...values,
-                },
-              },
-            });
-          });
-        }}
-        status={user.profile.status}
-        url={user.profile.url}
-        username={user.details.username}
-      />
+      <Grid container justify="center">
+        <Grid item md={3} sm={8} xs={12}>
+          <ProfileCard
+            bio={user.profile.bio}
+            firstName={user.details.name.split(' ')[0]}
+            isOwner={this.props.myId === user._id}
+            joinDate={new Date(parseInt(user._id.substring(0, 8), 16) * 1000)}
+            lastName={user.details.name.split(' ')[1]}
+            onSubmit={(values, { setSubmitting }): void => {
+              const array: Array<[string, Record<string, unknown>]> = [];
+              for (const [key, value] of Object.entries(values)) {
+                array.push(['profile.' + key, { [key]: value }]);
+              }
+              Axios.put('/api/user/set', {
+                updates: array,
+              }).then((res) => {
+                setSubmitting(false);
+                this.setState({
+                  user: {
+                    ...user,
+                    profile: {
+                      ...user.profile,
+                      ...values,
+                    },
+                  },
+                });
+              });
+            }}
+            status={user.profile.status}
+            url={user.profile.url}
+            username={user.details.username}
+          />
+        </Grid>
+        <Grid item md={6}>
+          {agoragrams.map(({ _id }: Agoragram) => (
+            <ReduxConnectedPost _id={_id} key={_id} longPostIsFadedOut />
+          ))}
+        </Grid>
+      </Grid>
     );
   }
 }
